7482af6ee8b5510f107a4396136de980f93edc82
namdang0219 <namdang.contact@gmail.com>
2025-02-28 15:55:03 +0900

    get all users from data base and log string to view

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,9 @@
 const connection = require("../config/database");
+const  { getAllUsers } = require('../services/CRUDService')
 
-const getHomepage = (req, res) => {
-	return res.render("home.ejs");
+const getHomepage = async (req, res) => {
+	const results = await getAllUsers();
+	return res.render("home.ejs", { listUsers: results });
 };
 
 const getCreatePage = (req, res) => {
@@ -18,13 +20,13 @@ const getNamdang = (req, res) => {
 
 const postCreateUser = async (req, res) => {
 	let { email, name, city } = req.body;
-    
+
 	const [results, fields] = await connection.query(
 		`INSERT INTO Users (email, name, city)
 	            VALUES (?, ?, ?);`,
 		[email, name, city]
 	);
-    res.send("Create user successfully")
+	res.send("Create user successfully");
 };
 
 module.exports = {
