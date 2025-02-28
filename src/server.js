9a2690d6e201d2e8b63b4e1d62e7fe2176340e7f
namdang0219 <namdang.contact@gmail.com>
2025-02-28 20:15:23 +0900

    create new project with express installed

--- src/server.js
+++ src/server.js
@@ -1,25 +1,11 @@
-require("dotenv").config();
-const express = require("express");
-const configViewEngine = require("./config/viewEngine");
-const webRoutes = require("./routes/web");
+const express = require('express')
+const app = express()
+const port = 3001
 
-const app = express(); // Define app
-const port = process.env.PORT || 8888;
-const hostname = process.env.HOST_NAME;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// config template engine
-configViewEngine(app);
-
-// config req.body
-app.use(express.json()); // for json
-app.use(express.urlencoded({ extended: true })); // for urlencoded
-
-app.use("/", webRoutes); 
-/**
- * first parameter is default path before 2nd parameter
- * used for api versioning
- */
-
-app.listen(port, hostname, () => {
-	console.log("listening on port " + port);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
